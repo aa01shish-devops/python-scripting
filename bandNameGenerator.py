a652934cc665a19f6f7ce337809a56d394eb302f
aa01shish-devops <procscript@gmail.com>
2026-05-19 12:05:52 +0530

    Project: Band Name  generator, #Day1 #100daysofcode

---
+++ bandNameGenerator.py
@@ -0,0 +1,6 @@
+userName = input(f'Welcome To Band Name Generator\nWhat\'s the name of the city you grew in?\n')
+petName = input(f'What\'s your pet\'s name?\n')
+
+
+print(f'Your Band Name Could be {userName} {petName}')
+
