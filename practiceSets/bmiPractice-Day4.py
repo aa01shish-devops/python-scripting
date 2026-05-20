06988b6e056e458726e65460a446703347c0ae93
aa01shish-devops <procscript@gmail.com>
2026-05-20 10:45:10 +0530

    Added BMI Calculator as Practice Set Day4

---
+++ practiceSets/bmiPractice-Day4.py
@@ -0,0 +1,23 @@
+'''
+    BMI Calculator
+The body mass index (BMI) is a measure used in medicine to see if someone is underweight or overweight. This is the formula used to calculate it:
+
+
+
+bmi is equal to the person's weight divided by the person's height squared.
+
+
+
+Convert this sentence into code on line 6.
+
+
+'''
+
+height = float(1.67) 
+weight = int(82)
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = float(weight / (height ** 2))
+
+print(bmi)
