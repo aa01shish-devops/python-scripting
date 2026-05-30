e1d93c5ea71d6ca7ed6f41442ac770862b4e373e
aa01shish-devops <procscript@gmail.com>
2026-05-30 17:38:03 +0530

    Small Program For List

---
+++ practiceSets/knowAboutLists.py
@@ -0,0 +1,38 @@
+indian_states = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+]
+
+sortedIndianStates = sorted(indian_states , reverse=True)
+
+print("The Names of Indian States in ascending order are:") 
+
+for index, name in enumerate(sortedIndianStates):
+    print(f'[{index}]: {name} ')
+
